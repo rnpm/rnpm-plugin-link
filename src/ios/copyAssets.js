cbac2d5bdaa04b5f5cc42b8eeea91eda7832b93a
Mike Grabowski <grabbou@gmail.com>
2015-11-27 08:47:26 +0100

    Implement new copyAssets logic
    Previously, files has just been copied (Android only) no matter its
    type. Now, we not only perform the same for iOS, but we no longer only
    copy the files.
    
    That is, starting from now, for Android:
    - We copy all the things to assetsPath (src/main/assets) based on the
      file type (e.g. fonts go to `fonts` subolder)
    
    And for iOS
    - We add files to `Resources` group, add them to `Copy bundle resources`
      section
    - Update Info.plist with UIAppFonts (so called Fonts provided by
      application)

---
+++ src/ios/copyAssets.js
@@ -0,0 +1,63 @@
+const fs = require('fs-extra');
+const utils = require('../utils');
+const path = require('path');
+const xcode = require('xcode');
+const log = require('npmlog');
+const plistParser = require('plist');
+
+/**
+ * This function works in a similar manner to its Android version,
+ * except it does not copies fonts but creates XCode Group references
+ */
+module.exports = function copyAssetsIOS(files, projectConfig) {
+  const project = xcode.project(projectConfig.pbxprojPath).parseSync();
+  const assets = utils.groupByType(files);
+  const plistPath = path.join(
+    projectConfig.sourceDir,
+    project.getBuildProperty('INFOPLIST_FILE')
+  );
+
+  if (!fs.existsSync(plistPath)) {
+    return log.error(
+      'ERRPLIST',
+      `Could not locate Info.plist file at ${plistPath}. Check if your project has Info.plist set properly`
+    );
+  }
+
+  if (!project.pbxGroupByName('Resources')) {
+    return log.error(
+      'ERRGROUP',
+      `Group 'Resources' does not exist in your XCode project. See ` +
+      `https://developer.apple.com/library/ios/recipes/xcode_help-structure_navigator/articles/Creating_a_Group.html ` +
+      `for instructions on setting it up.`
+    );
+  }
+
+  const plist = plistParser.parse(
+    fs.readFileSync(plistPath, 'utf-8')
+  );
+
+  const fonts = (assets.font || [])
+    .map(asset =>
+      project.addResourceFile(
+        path.relative(projectConfig.sourceDir, asset),
+        {
+          target: project.getFirstTarget().uuid,
+        }
+      )
+    )
+    .filter(file => file)   // xcode returns false if file is already there
+    .map(file => file.basename);
+
+  plist.UIAppFonts = (plist.UIAppFonts || []).concat(fonts);
+
+  fs.writeFileSync(
+    projectConfig.pbxprojPath,
+    project.writeSync()
+  );
+
+  fs.writeFileSync(
+    plistPath,
+    plistParser.build(plist)
+  );
+};
