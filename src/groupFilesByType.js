59ed8f6a0b6a94dca4fbcaeac512bde3466f0c16
Mike Grabowski <grabbou@gmail.com>
2015-11-30 13:54:29 +0100

    Clarify groupByType method

--- src/groupFilesByType.js
+++ src/groupFilesByType.js
@@ -11,6 +11,17 @@ mime.define({
   'font/truetype': ['ttf'],
 });
 
+/**
+ * Given an array of files, it groups it by it's type.
+ * Type of the file is inferred from it's mimetype based on the extension
+ * file ends up with. The returned value is an object with properties that
+ * correspond to the first part of the mimetype, e.g. images will be grouped
+ * under `image` key since the mimetype for them is `image/jpg` etc.
+ *
+ * Example:
+ * Given an array ['fonts/a.ttf', 'images/b.jpg'],
+ * the returned object will be: {font: ['fonts/a.ttf'], image: ['images/b.jpg']}
+ */
 module.exports = function groupFilesByType(assets) {
   return groupBy(assets, type => mime.lookup(type).split('/')[0]);
 };
