8b805d06a8835ba3f6cefd901d62a1fddd79eee6
Mike Grabowski <grabbou@gmail.com>
2016-01-09 11:30:45 +0100

    Add in unregister part

--- src/ios/unregisterNativeModule.js
+++ src/ios/unregisterNativeModule.js
@@ -1,1 +1,22 @@
-module.exports = function noop() {};
+const xcode = require('xcode');
+
+const hasLibraryImported = require('./hasLibraryImported');
+
+/**
+ * Unregister native module IOS
+ *
+ * If library is already unlinked, this action is a no-op.
+ */
+module.exports = function unregisterNativeModule() {
+  const project = xcode.project(projectConfig.pbxprojPath).parseSync();
+  const dependencyProject = xcode.project(dependencyConfig.pbxprojPath).parseSync();
+
+  const libraries = project.pbxGroupByName(projectConfig.libraryFolder);
+  if (!hasLibraryImported(libraries, dependencyConfig.projectName)) {
+    return false;
+  }
+
+  // @todo add actual logic here later
+
+  return true;
+};
