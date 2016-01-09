af60cffe7fd373c194bdc75d72acc713b2e923d6
Mike Grabowski <grabbou@gmail.com>
2016-01-09 11:19:37 +0100

    Add in some comments

--- src/ios/getPlistPath.js
+++ src/ios/getPlistPath.js
@@ -1,3 +1,8 @@
+/**
+ * Returns path to Info.plist located in the iOS project and removes
+ * xcode $(SRCROOT) and other implementation specific decorators, so that
+ * return value can be easily used in e.g. path.join calls
+ */
 module.exports = function getPlistPath(project) {
   return project.getBuildProperty('INFOPLIST_FILE')
     .replace(/"/g, '')
