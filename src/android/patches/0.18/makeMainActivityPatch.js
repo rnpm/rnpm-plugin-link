801ae2fd0d436212870729207846835e0542a1c6
Kureev Alexey <kureev-mail@ya.ru>
2016-01-25 21:43:50 +0100

    Make it work react-native 0.18

---
+++ src/android/patches/0.18/makeMainActivityPatch.js
@@ -0,0 +1,26 @@
+const append = (scope, pattern, patch) =>
+  scope.replace(pattern, `${pattern}${patch}`);
+
+module.exports = function makeMainActivityPatch(config) {
+  const importPattern = 'import com.facebook.react.ReactActivity;';
+  const packagePattern = 'new MainReactPackage()';
+  /**
+   * Make a MainActivity.java program patcher
+   * @param  {String}   importPath Import path, e.g. com.oblador.vectoricons.VectorIconsPackage;
+   * @param  {String}   instance   Code to instance a package, e.g. new VectorIconsPackage();
+   * @return {Function}            Patcher function
+   */
+  return function applyMainActivityPatch(content) {
+    const patched = append(
+      content,
+      importPattern,
+      '\n' + config.packageImportPath
+    );
+
+    return append(
+      patched,
+      packagePattern,
+      ',\n        ' + config.packageInstance
+    );
+  };
+};
--- src/android/registerNativeModule.js
+++ src/android/registerNativeModule.js
@@ -1,14 +1,66 @@
 const path = require('path');
 const semver = require('semver');
+const fs = require('fs-extra');
+const compose = require('lodash.flowright');
 
-module.exports = function registerNativeModule(name, dependencyConfig, projectConfig) {
+const readFile = (file) =>
+  () => fs.readFileSync(file, 'utf8');
+
+const writeFile = (file, content) => content ?
+  fs.writeFileSync(file, content, 'utf8') :
+  (c) => fs.writeFileSync(file, c, 'utf8');
+
+module.exports = function registerNativeAndroidModule(name, dependencyConfig, projectConfig) {
   const rnpkg = require(
     path.join(projectConfig.folder, 'node_modules', 'react-native', 'package.json')
   );
 
+  var prefix = 'patches/0.18';
+
   if (semver.lt(rnpkg.version, '0.18.0')) {
-    return require('./patches/0.17').apply(null, arguments);
+    prefix = 'patches/0.17';
   }
 
-  return require('./patches/0.18').apply(null, arguments);
+  const makeSettingsPatch = require(`./${prefix}/makeSettingsPatch`);
+  const makeBuildPatch = require(`./${prefix}/makeBuildPatch`);
+  const makeMainActivityPatch = require(`./${prefix}/makeMainActivityPatch`);
+
+  const applySettingsPatch = makeSettingsPatch.apply(null, arguments);
+  const applyBuildPath = makeBuildPatch(name);
+  const applyMainActivityPatch = makeMainActivityPatch(dependencyConfig);
+
+  const performSettingsGradlePatch = compose(
+    writeFile(projectConfig.settingsGradlePath),
+    applySettingsPatch,
+    readFile(projectConfig.settingsGradlePath)
+  );
+
+  const performBuildGradlePatch = compose(
+    writeFile(projectConfig.buildGradlePath),
+    applyBuildPath,
+    readFile(projectConfig.buildGradlePath)
+  );
+
+  const performMainActivityPatch = compose(
+    writeFile(projectConfig.mainActivityPath),
+    applyMainActivityPatch,
+    readFile(projectConfig.mainActivityPath)
+  );
+
+  /**
+   * Check if module has been installed already
+   */
+  const isInstalled = compose(
+    (content) => ~content.indexOf(dependencyConfig.packageInstance),
+    readFile(projectConfig.mainActivityPath)
+  );
+
+  if (!isInstalled(name)) {
+    compose(
+      performSettingsGradlePatch,
+      performBuildGradlePatch,
+      performMainActivityPatch
+    )();
+  }
 };
+
