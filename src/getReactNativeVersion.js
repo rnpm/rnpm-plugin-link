a5fc0fe2626e9e0ca9ce4262d56765bb0094e695
Mike Grabowski <grabbou@gmail.com>
2016-01-29 19:53:10 +0100

    Oops, btw- why test do not throw here? Too much mocked imo

--- src/getReactNativeVersion.js
+++ src/getReactNativeVersion.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 module.exports = (folder) => require(
-  path.join(folder, 'node_modules', 'react-native', 'package.json').version
-);
+  path.join(folder, 'node_modules', 'react-native', 'package.json')
+).version;
