9db14ec3de8dc755c46ada6aaf524951647027b6
Mike Grabowski <grabbou@gmail.com>
2016-01-29 20:40:22 +0100

    Going to make that change in separate PR later

--- src/android/getPrefix.js
+++ src/android/getPrefix.js
@@ -2,7 +2,11 @@ const semver = require('semver');
 
 module.exports = function getPrefix(rnVersion) {
   const version = rnVersion.replace('-rc', '');
-  return semver.satisfies(version, '>= 0.18.0')
-    ? 'patches/0.18'
-    : 'patches/0.17';
+  var prefix = 'patches/0.18';
+
+  if (semver.lt(version, '0.18.0')) {
+    prefix = 'patches/0.17';
+  }
+
+  return prefix;
 };
