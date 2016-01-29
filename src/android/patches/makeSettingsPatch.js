d6338107ba4295cfac60efbf022f1911e9ba7c04
Mike Grabowski <grabbou@gmail.com>
2016-01-29 19:05:39 +0100

    Unit test & tweak

--- src/android/patches/makeSettingsPatch.js
+++ src/android/patches/makeSettingsPatch.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = function makeSettingsPatch(name, dependencyConfig, projectConfig) {
   const relative = path.relative(
-    projectConfig.sourceDir,
-    dependencyConfig.settingsGradlePath
+    path.dirname(projectConfig.settingsGradlePath),
+    dependencyConfig.sourceDir
   );
 
   /**
