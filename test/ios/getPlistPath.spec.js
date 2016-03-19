5fb522857c9616f6e75d986848ead9505d197548
Mike Grabowski <grabbou@gmail.com>
2016-03-19 22:27:49 +0100

    Remove test case, the error was sth else we will cover tmrw

--- test/ios/getPlistPath.spec.js
+++ test/ios/getPlistPath.spec.js
@@ -4,22 +4,16 @@ const xcode = require('xcode');
 const getPlistPath = require('../../src/ios/getPlistPath');
 
 const project = xcode.project('test/fixtures/project.pbxproj');
-const invalidProject = xcode.project('test/fixtures/projectWithoutPlist.pbxproj');
 
 describe('ios::getPlistPath', () => {
 
   beforeEach(() => {
     project.parseSync();
-    invalidProject.parseSync();
   });
 
   it('should return path without Xcode $(SRCROOT)', () => {
     const plistPath = getPlistPath(project);
     expect(plistPath).to.equals('Basic/Info.plist');
-  });
-
-  it('should return null when Info.plist is not set', () => {
-    expect(getPlistPath(invalidProject)).to.equals(null);
   });
 
 });
