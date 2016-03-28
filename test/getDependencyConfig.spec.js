6ee6c793347f549a067523e1923b2e796f1391c3
Mike Grabowski <grabbou@gmail.com>
2016-03-28 23:29:32 +0200

    Tests in progress

---
+++ test/getDependencyConfig.spec.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getDependencyConfig = require('../src/getDependencyConfig');
+
+describe('getDependencyConfig', () => {
+  it.skip('should return an array of dependencies\' rnpm config', () => {
+
+  });
+
+  it.skip('should filter out invalid react-native projects', () => {
+
+  });
+});
