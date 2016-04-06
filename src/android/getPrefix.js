5d6d2d17af7134adc44e330147583c5ea1f2ef56
Geoffrey Goh <kg422@cornell.edu>
2016-04-06 10:40:26 -0700

    Update getPrefix.js

--- src/android/getPrefix.js
+++ src/android/getPrefix.js
@@ -2,7 +2,7 @@ const semver = require('semver');
 const versions = ['0.20', '0.18', '0.17'];
 
 module.exports = function getPrefix(rnVersion) {
-  const version = rnVersion.substring(0, rnVersion.substring('-'));
+  const version = rnVersion.substring(0, rnVersion.indexOf('-'));
   var prefix = 'patches/0.20';
 
   versions.forEach((item, i) => {
