656955e3ef01c36b99766ac3b50caaa26c784a9f
Mike Grabowski <grabbou@gmail.com>
2016-04-06 21:52:52 +0200

    Add docs

--- src/ios/removeFromProjectReferences.js
+++ src/ios/removeFromProjectReferences.js
@@ -1,3 +1,13 @@
+/**
+ * For each file (.xcodeproj), there's an entry in `projectReferences` created
+ * that has two entries - `ProjectRef` - reference to a file.uuid and
+ * `ProductGroup` - uuid of a Products group.
+ *
+ * When projectReference is found - it's deleted and the removed value is returned
+ * so that ProductGroup in PBXGroup section can be removed as well.
+ *
+ * Otherwise returns null
+ */
 module.exports = function removeFromProjectReferences(project, file) {
   const firstProject = project.getFirstProject().firstProject;
 
