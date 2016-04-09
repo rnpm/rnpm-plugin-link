e1eaf44b343492416f22d669908efd20666f9e3e
Mike Grabowski <grabbou@gmail.com>
2016-04-09 18:08:33 +0200

    Remove old files

--- src/android/isInstalled.js
+++
@@ -1,9 +0,0 @@
-const compose = require('lodash').flowRight;
-const readFile = require('./fs').readFile;
-
-module.exports = function isInstalled(projectConfig, name) {
-  return compose(
-    (content) => ~content.indexOf(`:${name}`),
-    readFile(projectConfig.buildGradlePath)
-  )();
-};
