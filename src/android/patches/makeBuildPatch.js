c5db205609cd875c860b7cd388b6c81cec0833e1
Mike Grabowski <grabbou@gmail.com>
2016-04-10 17:33:54 +0200

    Revert "Fix unlink - do the same for build.gradle just in case"
    This reverts commit 046336d7822ec3d778ed134cfd47c88ac1506e6f.

--- src/android/patches/makeBuildPatch.js
+++ src/android/patches/makeBuildPatch.js
@@ -1,17 +1,13 @@
 module.exports = function makeBuildPatch(name) {
-  const patch = `    compile project(':${name}')`;
-
   /**
    * Replace pattern by patch in the passed content
    * @param  {String} content Content of the build.gradle file
    * @return {String}         Patched content of build.gradle
    */
-  const applyBuildPatch = (content) => {
+  return function applyBuildPatch(content) {
     const pattern = `dependencies {`;
+    const patch = `    compile project(':${name}')`;
+
     return content.replace(pattern, `${pattern}\n${patch}`);
   };
-
-  applyBuildPatch.patch = patch;
-
-  return applyBuildPatch;
 };
