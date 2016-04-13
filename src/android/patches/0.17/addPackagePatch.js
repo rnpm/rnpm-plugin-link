f707498054e17bbd138061236f2c9c5146e10b18
Kureev Alexey <kureev-mail@ya.ru>
2016-04-13 10:58:07 +0200

    Update patch structure for legacy versions

--- src/android/patches/0.17/addPackagePatch.js
+++
@@ -1,2 +0,0 @@
-module.exports = (config) =>
-  `                .addPackage(${config.packageInstance})`;
---
+++ src/android/patches/0.17/makePackagePatch.js
@@ -0,0 +1,13 @@
+module.exports = function makePackagePatch(packageInstance, params) {
+  const processedInstance = packageInstance.replace(
+    /\$\{(\w+)\}/g,
+    (pattern, paramName) => params[paramName]
+      ? `this.getResources().getString(R.strings.${paramName})`
+      : null
+  );
+
+  return {
+    pattern: '.addPackage(new MainReactPackage())',
+    patch: `\n                .addPackage(${processedInstance})`
+  };
+};
